3461327496fa431dc1e99fea775aaded204d040a
Akraftt <antonl29@hotmail.com>
2022-12-11 21:39:55 +0100

    fixed erorrs 400th time

--- Components/BookListItem.js
+++ Components/BookListItem.js
@@ -1,20 +1,8 @@
-const BookInformation = (book) => {
-  let html = `
-  <div id="bookDetail" class="book__details absolute inset-y right-12 rounded-md bg-white/40 p-4 w-1/5 top-20 ml-5 mx-auto 
-  flex flex-col text-center text-gray-700 font-semibold">
-      <img src="${book.coverImage}" alt="Omslaget för denna bok saknas">
-      
-      <div class="border"></div>  
-      <div class="detailed__list">
-          <ul class="list">
-              <li>Title: ${book.title}</li>
-              <li>Author: ${book.author}</li>
-              <li>Pages: ${book.pages}</li>
-              <li>Release date: ${book.releaseDate}</li>
-          </ul>
-      </div> 
-  </div>
-  `;
-
-  return html;
-};
+const BookListItem = (book) => {
+    let html = `<li id="${book.id}"
+                  class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-900 cursor-pointer">
+                ${book.author} - ${book.title} 
+                </li>`;
+    return html;
+  };
+  
